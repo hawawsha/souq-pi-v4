79ca45e1b7441e16d013b50e782d11aae92b14c2
AHMAD SULEIMAN MUSALLAM AlHawawsheh <38798371+hawawsha@users.noreply.github.com>
2026-07-01 00:22:26 +0300

    Fix import path for Product model in update-product.js

--- pages/api/admin/update-product.js
+++ pages/api/admin/update-product.js
@@ -1,5 +1,5 @@
+import { Product } from "../../../lib/models";
 import { connectDB } from "../../../lib/db";
-import Product from "../../../models/Product";
 
 export default async function handler(req, res) {
   if (req.method !== "PUT") {
